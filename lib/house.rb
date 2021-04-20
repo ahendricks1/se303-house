3c7321893840e69051c91bf6783056182cf0f0b1
Austin Hendricks <hendraus@oregonstate.edu>
2021-04-20 13:08:38 -0700

    Test test_line_2

--- lib/house.rb
+++ lib/house.rb
@@ -1,7 +1,11 @@
 class House
 
   def line(number)
-    "This is the house that Jack built.\n"
+    if number == 1
+      "This is the house that Jack built.\n"
+    else
+      "This is the malt that lay in the house that Jack built.\n"
+    end
   end
 
 end
