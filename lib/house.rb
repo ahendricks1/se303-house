ca7fcb8fb2248661173054cbcf43afb4ceeb5280
Austin Hendricks <hendraus@oregonstate.edu>
2021-04-20 13:12:59 -0700

    Change conditional from if/else to case statement

--- lib/house.rb
+++ lib/house.rb
@@ -1,11 +1,12 @@
 class House
 
   def line(number)
-    if number == 1
+    case number
+    when 1
       "This is the house that Jack built.\n"
-    elsif number == 2
+    when 2
       "This is the malt that lay in the house that Jack built.\n"
-    else
+    when 3
       "This is the rat that ate the malt that lay in the house that Jack built.\n"
     end
   end
