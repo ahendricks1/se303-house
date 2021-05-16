b87c9303fca12320e95130664aa0aca6dd452bbc
Austin Hendricks <hendraus@oregonstate.edu>
2021-05-16 12:25:38 -0700

    Add initial pirate version test

--- test/house_test.rb
+++ test/house_test.rb
@@ -94,4 +94,9 @@ This is the horse and the hound and the horn that belonged to the farmer sowing 
     assert_equal expected, House.new.recite
   end
 
+  def test_thar_line_1
+    expected = "Thar be the house that Jack built.\n"
+    assert_equal expected, House.new.line(1, true)
+  end
+
 end
