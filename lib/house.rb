b4b6e9dcde758fdd8840b154031a23aab9f78845
Austin Hendricks <hendraus@oregonstate.edu>
2021-05-16 12:05:40 -0700

    Add phrase and replace case 11 with lines function

--- lib/house.rb
+++ lib/house.rb
@@ -2,6 +2,7 @@ class House
 
   def phrases
     [
+      "farmer sowing his corn that kept the",
       "rooster that crowed in the morn that woke the",
       "priest all shaven and shorn that married the",
       "man all tattered and torn that kissed the",
@@ -43,7 +44,7 @@ class House
     when 10
       lines(number)
     when 11
-      "This is the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+      lines(number)
     when 12
       "This is the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
     end
