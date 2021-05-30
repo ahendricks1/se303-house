a82501bedaecd554ec03dcddff0671a7d39fb996
Austin Hendricks <hendraus@oregonstate.edu>
2021-05-30 13:03:09 -0700

    Shuffle subject and verb arrays

--- lib/house.rb
+++ lib/house.rb
@@ -34,22 +34,6 @@ class House
     ]
   end
 
-  def phrases
-    [
-      "the horse and the hound and the horn that belonged to ",
-      "the farmer sowing his corn that kept ",
-      "the rooster that crowed in the morn that woke ",
-      "the priest all shaven and shorn that married ",
-      "the man all tattered and torn that kissed ",
-      "the maiden all forlorn that milked ",
-      "the cow with the crumpled horn that tossed ",
-      "the dog that worried ",
-      "the cat that killed ",
-      "the rat that ate ",
-      "the malt that lay in ",
-    ]
-  end
-
   def prefix
     "This is"
   end
@@ -77,7 +61,11 @@ end
 
 class RandomHouse < House
 
-  def phrases
+  def subjects
+    super.shuffle!
+  end
+
+  def verbs
     super.shuffle!
   end
 
@@ -94,8 +82,11 @@ class RandomPirateHouse < House
     @pirate_template.prefix
   end
 
-  def phrases
-    @random_template.phrases
+  def subjects
+    @random_template.subjects
   end
 
+  def verbs
+    @random_template.verbs
+  end
 end
