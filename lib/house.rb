9fbf9ff84972258bde0448ece7d93dcde1d19258
Austin Hendricks <hendraus@oregonstate.edu>
2021-05-30 11:14:57 -0700

    Move 'the house that Jack built' into the line function

--- lib/house.rb
+++ lib/house.rb
@@ -1,18 +1,17 @@
 class House
 
   @@phrases = [
-    "horse and the hound and the horn that belonged to the",
-    "farmer sowing his corn that kept the",
-    "rooster that crowed in the morn that woke the",
-    "priest all shaven and shorn that married the",
-    "man all tattered and torn that kissed the",
-    "maiden all forlorn that milked the",
-    "cow with the crumpled horn that tossed the",
-    "dog that worried the",
-    "cat that killed the",
-    "rat that ate the",
-    "malt that lay in the",
-    "house that Jack built"
+    "the horse and the hound and the horn that belonged to ",
+    "the farmer sowing his corn that kept ",
+    "the rooster that crowed in the morn that woke ",
+    "the priest all shaven and shorn that married ",
+    "the man all tattered and torn that kissed ",
+    "the maiden all forlorn that milked ",
+    "the cow with the crumpled horn that tossed ",
+    "the dog that worried ",
+    "the cat that killed ",
+    "the rat that ate ",
+    "the malt that lay in ",
   ]
 
   def prefix
@@ -20,7 +19,7 @@ class House
   end
 
   def line(number)
-    "#{prefix} the #{@@phrases.last(number).join(" ")}.\n"
+    "#{prefix} #{@@phrases.last(number - 1).join("")}the house that Jack built.\n"
   end
 
   def recite
@@ -39,7 +38,7 @@ end
 class RandomHouse < House
 
   def line(number)
-    "#{prefix} the #{@@phrases.slice(0, 11).sample(number).join(" ")} house that Jack built.\n"
+    "#{prefix} #{@@phrases.slice(0, 11).sample(number).join("")}the house that Jack built.\n"
   end
 
 end
@@ -55,7 +54,7 @@ class RandomPirateHouse < House
   end
 
   def line(number)
-    "#{prefix} the #{@@phrases.slice(0, 11).sample(number).join(" ")} house that Jack built.\n"
+    "#{prefix} #{@@phrases.slice(0, 11).sample(number).join("")}the house that Jack built.\n"
   end
 
 end
